565d16778859900ae9bbf9aac7c59e225f6445a3
Visual Dudio <omar.mbarki5@gmail.com>
2017-10-08 18:26:58 -0500

    Set up intial Xamarin.Forms project. Added XAML page skeletons and helpful comments underlining how the app should be built. Also added the Song model that the API team will be using.

---
+++ Liddup/Liddup/Models/Song.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liddup.Models
+{
+    class Song
+    {
+        public Song() : this("", "") { }
+
+        public Song(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public Dictionary<string, object> ToDictionary()
+        {
+            var dictionary = new Dictionary<string, object>
+            {
+                {"title", Title },
+                {"description", Description }
+            };
+
+            return dictionary;
+        }
+    }
+}
